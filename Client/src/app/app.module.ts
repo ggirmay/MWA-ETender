4081d68470615c80b443f062572bd57a57c36eff
ggirmay <ggeberselassie@mum.edu>
2020-03-14 14:26:23 -0500

    merge

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,21 +3,16 @@ import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
-<<<<<<< HEAD
+
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 
-=======
-import {MatButtonModule} from '@angular/material/button';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 import { PostComponent } from './post.component';
->>>>>>> 9e16fd5091cec69893f723d2eb88d5a96f6a15e0
 
 @NgModule({
   declarations: [
@@ -31,12 +26,9 @@ import { PostComponent } from './post.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatInputModule,
-<<<<<<< HEAD
     FormsModule,
     MatCheckboxModule
-=======
-    MatButtonModule
->>>>>>> 9e16fd5091cec69893f723d2eb88d5a96f6a15e0
+
     
   ],
   providers: [],
