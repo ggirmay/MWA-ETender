7685be1921157049c699536cf1e4933a01704f44
ggirmay <ggeberselassie@mum.edu>
2020-03-17 09:50:46 -0500

    style test

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: []
+  styles: [`.fixed-header{font-size : 20px}`]
 })
+
 export class AppComponent {
 
   constructor() {}
