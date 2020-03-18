1a7c6a81c6de6525bfc4a888bb4f06ea8a38a8f4
ggirmay <ggeberselassie@mum.edu>
2020-03-18 09:53:05 -0500

    catagory

--- Client/src/app/register.component.ts
+++ Client/src/app/register.component.ts
@@ -3,39 +3,27 @@ import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { RegService } from './reg.service';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
 template: `
   <h3>Registering a Bidder</h3>
+  <input type='checkbox' onchange='handleChange(this);'>Bidder
+  <input type='checkbox' onchange='handleChange(this);'>Client<br/>
   <form [formGroup]="regForm" (ngSubmit)="onSubmit()">
 
-<mat-form-field>
-  <mat-label> Select an option </mat-label>
-  <mat-select[(value)] = "selectedValue multiple "
-  
-  <mat-option value="art">Art</mat-option>
-  <mat-option value="entertainment">Entertainment</mat-option>
-  <mat-option value="music">Music</mat-option>
-  <mat-option value="IT">IT</mat-option>
-  </mat-select> 
-  </mat-form-field><br/>
-
-
-
   
 
-  
-
-  <mat-form-field >
+  <mat-form-field>
+    <mat-label>Company Name</mat-label>
+    <input matInput placeholder="cname" type="text" formControlName="cname"/>
+   </mat-form-field><br>
+  <mat-form-field>
     <mat-label>Lisence Number</mat-label>
     <input matInput placeholder="lnumber" type="text" formControlName="lnumber"/>
    </mat-form-field><br>
-  <mat-form-field >
-    <mat-label>Catagory</mat-label>
-    <input matInput placeholder="catagory" type="text" formControlName="catagory"/>
-  </mat-form-field><br/>
-  <mat-form-field >
+    <mat-form-field >
     <mat-label>Email</mat-label>
     <input matInput placeholder="email" type="text" formControlName="email"/>
   </mat-form-field><br/>
@@ -50,6 +38,16 @@ template: `
   <mat-form-field >
   <mat-label>Password</mat-label>
     <input matInput placeholder="pwd" type="password" formControlName="pwd"/>
+  </mat-form-field><br/>
+
+  <mat-form-field>
+  <mat-label>Catagory</mat-label><br/>
+  <mat-select formControlName="catagory" multiple>
+  <mat-option value="art">Art</mat-option>
+  <mat-option value="entertainment">Entertainment</mat-option>
+  <mat-option value="music">Music</mat-option>
+  <mat-option value="IT">IT</mat-option>
+  </mat-select>
   </mat-form-field><br/>
   <button>send</button>
 
@@ -67,17 +65,20 @@ export class RegisterComponent implements OnInit {
     this.regForm=fb.group({
       'cname':['', Validators.required],
       'lnumber':['', Validators.required],
-      'catagory':['', Validators.required],
+      'usertype':['', Validators.required],
+      'catagory': ['', Validators.required],
       'email':['', Validators.required],
       'location':[''],
       'uname':['', Validators.required],
       'pwd':['', Validators.required]
-    })
-        //   'catagory':this.postForm.value.catagory,
+    })}
+        
     //   'deadline':this.postForm.value.deadline
     // }
-    console.log('data of form= '+JSON.stringify(formValue));
-    this._regService.regClient(formValue)
+    onSubmit(){
+    
+    console.log('data of form= '+JSON.stringify(this.regForm.value));
+    this._regService.regClient(this.regForm.value)
     .subscribe(
       res=> {
         console.log(res),
@@ -86,6 +87,15 @@ export class RegisterComponent implements OnInit {
       err=> console.log(err) 
   )
     }
+
+    handleChange(checkbox) {
+      if(checkbox.checked == true){
+          document.getElementById("catagory").removeAttribute("disabled");
+      }else{
+          document.getElementById("submit").setAttribute("disabled", "disabled");
+     }
+  }
+    
   ngOnInit(): void {
     throw new Error("Method not implemented.");
   }
@@ -126,4 +136,52 @@ export class RegisterComponent implements OnInit {
 //   }
 
 // }
+
+
+// <mat-form-field>
+// <mat-radio-group #reg>
+// <mat-label> Select an option </mat-label>
+// <mat-radio-button value ="client">Client</mat-radio-button>
+// <mat-radio-button value ="bidder">Bidder</mat-radio-button>
+// <mat-radio-group>
+// </mat-form-field>
+
+// <mat-form-field">
+// <mat-label> Select an option </mat-label>
+// <mat-select[(value)] = "selectedValue multiple "
+// <mat-option>None</mat-option>
+// <mat-option value="art">Art</mat-option>
+// <mat-option value="entertainment">Entertainment</mat-option>
+// <mat-option value="music">Music</mat-option>
+// <mat-option value="IT">IT</mat-option>
+// </mat-select> 
+// </mat-form-field><br/>
+
+
+// <mat-form-field>
+// <mat-radio-group #reg>
+// <mat-label> Select an option </mat-label><br/>
+// <mat-radio-button value ="client">Client</mat-radio-button> <mat-radio-button value ="bidder">Bidder</mat-radio-button>
+// </mat-radio-group>
+// </mat-form-field><br/>
+
+// <mat-form-field >
+// <mat-label> Select an option </mat-label>
+// <mat-select [(value)] = "selectedValue" multiple>
+// <mat-option>None</mat-option>
+// <mat-option value="art">Art</mat-option>
+// <mat-option value="entertainment">Entertainment</mat-option>
+// <mat-option value="music">Music</mat-option>
+// <mat-option value="IT">IT</mat-option>
+// </mat-select> 
+// </mat-form-field><br/>
+
+
+// <mat-form-field>
+//   <input name="options"  [(ngModel)]="options" type="radio" [value]="true" [checked]="options"/> Bidder
+  
+//   <input name="options"  [(ngModel)]="options" type="radio" [value]="false" [checked]="!options"/> Client
+//   </mat-form-field><br>
+
+
 }
