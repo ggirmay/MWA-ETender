69eec34d05c3a717ecded772f7a93b03cb078246
BrukTekle <50760538+BrukTekle@users.noreply.github.com>
2020-03-19 12:16:26 -0700

    Update home.component.ts

--- Client/src/app/home/home.component.ts
+++ Client/src/app/home/home.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   
     <h1>WELCOME TO ETENDER</h1>
     <img  src="./assets/Bid_logo.png" style="width:250px;"> <h2>Bid Farewell To High Prices!!!</h2>
-    
   `,
   styles: ['img {float: left; width:80%;} ']
 })
